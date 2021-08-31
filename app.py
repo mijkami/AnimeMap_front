194b4461b70dc6a8741e3d971442e5ff59e799a8
Dimitri GAILLOT <dimitri.gaillot@gmail.com>
2021-08-31 15:48:24 +0200

    working on list handling

--- app.py
+++ app.py
@@ -3,21 +3,30 @@ import requests
 import numpy as np
 import pandas as pd
 
-
+    
 url_api ='http://0.0.0.0:8000/predict'
 
+def convert_dict_to_list(dict):
+    temp = []
+    dictList = []
+    for key, value in dict.items():
+        temp = [key,value]
+        dictList.append(temp)
+    return dictList
+
+
 st.markdown("""# Anime Map
-## This is a sub header
-This is text
+## Machine-learning based recommendation system
+Fill the cells and run them to get a recommendation!
 
 _______________
 """)
 
 
-anime_input = st.text_input('Give the name of an anime!"', value='Naruto')
-predict_size_input = st.number_input('Size of desired prediction list', value=10)
+anime_input = st.text_input('Name of the anime to predict on:', value='Naruto')
+predict_size_input = st.number_input('Size of desired prediction list:', value=10)
 model_input = st.selectbox(
-       'Which model do you want to use?',
+       'Model to use:',
        ('notation', 'completed'))
 params = {
     'anime' : anime_input,
@@ -27,12 +36,15 @@ params = {
 
 response = requests.get(url_api, params=params).json()
 
-prediction = response['prediction']
+
+
+prediction_list = convert_dict_to_list(response['prediction'])
+prediction_list_names = [prediction_list[i][0] for i in range(len(prediction_list))]
 
 st.markdown(f'''
 _______________
 
 ## You might want to watch these animes :
-${prediction}
+{prediction_list_names}
 _______________
 ''')
